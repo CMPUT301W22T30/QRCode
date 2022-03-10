8987a229cab6a0e826b41fbec53cd0c01fde3db6
tqdat712 <tqdat71201@gmail.com>
2022-03-10 12:01:47 -0700

    tqdat commit 2 10/03

---
+++ QRCodeTeam30/app/src/main/java/com/example/qrcodeteam30/listviewadapter/CustomListViewAllComment.java
@@ -0,0 +1,47 @@
+package com.example.qrcodeteam30.listviewadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.qrcodeteam30.R;
+import com.example.qrcodeteam30.modelclass.Comment;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class CustomListViewAllComment extends ArrayAdapter<Comment> {
+    private ArrayList<Comment> arrayList;
+    private Context context;
+
+    public CustomListViewAllComment(Context context, ArrayList<Comment> arrayList) {
+        super(context, 0, arrayList);
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        var view = convertView;
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.listview_viewallcomment_content, parent, false);
+        }
+
+        var comment = arrayList.get(position);
+        TextView textViewAuthor = view.findViewById(R.id.textViewAuthor_viewallcomment_listview_content);
+        TextView textViewDate = view.findViewById(R.id.textViewDate_viewallcomment_listview_content);
+        TextView textViewContent = view.findViewById(R.id.textViewContent_viewallcomment_listview_content);
+        textViewAuthor.setText(String.format(Locale.CANADA, "@%s", comment.getAuthor()));
+        textViewDate.setText(comment.getDate());
+        textViewContent.setText(comment.getContent());
+
+        return view;
+    }
+}
