8d321eb9593b7c7662abc2d1264d109086d16d33
JamesLe01 <quanganhle2001@gmail.com>
2022-03-11 22:21:02 -0700

    Commit with Demo Espresso Test

---
+++ QRCodeTeam30/app/src/androidTest/java/com/example/qrcodeteam30/DemoTest.java
@@ -0,0 +1,66 @@
+package com.example.qrcodeteam30;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class DemoTest {
+    @Test
+    public void testLogInSuccess() throws Exception {
+        // Launch MainActivity
+        ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
+
+        // Type "admin" to username editText
+        Espresso.onView(withId(R.id.signin_username_editText))
+                .perform(typeText("admin")).check(matches(withText("admin")));
+        // Type password
+        Espresso.onView(withId(R.id.signin_password_editText))
+                .perform(typeText("pwadmin"));
+
+        // Press button to sign in
+        Espresso.onView(withId(R.id.sign_in_button)).perform(click());
+
+        // Sleep 1 second (await for data from Firestore to download to device)
+        // Must sleep every time we switch activity (1 second is enough)
+        // The reason is because Espresso is too fast, so the new acitivity has not been registered
+        // yet after this code is run => Error
+        Thread.sleep(1000);
+
+        // Check if ranking button is displayed
+        Espresso.onView(withId(R.id.button_playerMenu_ranking)).check(matches(isDisplayed()));
+
+        scenario.close();
+    }
+
+    @Test
+    public void testMyProfile() throws Exception {
+        // Launch PlayerMenuActivity
+        // This activity has an input "SessionUsername"
+        // We simulate the input here
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), PlayerMenuActivity.class);
+        intent.putExtra("SessionUsername", "admin");
+        ActivityScenario scenario = ActivityScenario.launch(intent);
+
+        // Click on myProfile button to view my profile
+        Espresso.onView(withId(R.id.button_playerMenu_myProfile)).perform(click());
+
+        // Check if estimate ranking button is displayed
+        Espresso.onView(withId(R.id.button_myProfile_myRanking)).check(matches(isDisplayed()));
+
+        scenario.close();
+    }
+}
