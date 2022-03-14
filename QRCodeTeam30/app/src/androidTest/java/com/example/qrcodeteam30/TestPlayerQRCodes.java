8a93eae57996e1af91693acca26ee98ad8335505
tqdat712 <tqdat71201@gmail.com>
2022-03-13 22:33:55 -0600

    tqdat commit 1 13/03

--- QRCodeTeam30/app/src/androidTest/java/com/example/qrcodeteam30/TestPlayerQRCodes.java
+++ QRCodeTeam30/app/src/androidTest/java/com/example/qrcodeteam30/TestPlayerQRCodes.java
@@ -1,4 +1,122 @@
 package com.example.qrcodeteam30;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.anything;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class TestPlayerQRCodes {
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void testgenerateQRCode() throws Exception {
+        // Launch MainActivity
+        ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
+
+        // Type "admin" to username editText
+        Espresso.onView(withId(R.id.signin_username_editText))
+                .perform(typeText("admin"));
+        // Type password
+        Espresso.onView(withId(R.id.signin_password_editText))
+                .perform(typeText("pwadmin"));
+
+        // Press button to sign in
+        Espresso.onView(withId(R.id.sign_in_button)).perform(click());
+        Thread.sleep(1000);
+
+        // Press My Profile button to view QR Codes
+        Espresso.onView(withId(R.id.button_playerMenu_myProfile)).perform(click());
+        Thread.sleep(1000);
+
+        // Press Generate QR Code button
+        Espresso.onView(withId(R.id.button_myProfile_generateQRCode)).perform(click());
+        Thread.sleep(1000);
+
+        // Check the QR Code
+        Espresso.onView(withId(R.id.MyProfileImageView)).check(matches(isDisplayed()));
+
+        scenario.close();
+    }
+
+    @Test
+    public void loginQRCode() throws Exception {
+        // Launch MainActivity
+        ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
+
+        Espresso.onView(withId(R.id.sign_in_with_qrcode_button)).check(matches(isClickable()));
+        // Type "admin" to username editText
+        Espresso.onView(withId(R.id.signin_username_editText))
+                .perform(typeText("admin"));
+        // Type password
+        Espresso.onView(withId(R.id.signin_password_editText))
+                .perform(typeText("pwadmin"));
+
+        // Press button to sign in
+        Espresso.onView(withId(R.id.sign_in_button)).perform(click());
+        Thread.sleep(1000);
+
+        // Press My Profile button to view QR Codes
+        Espresso.onView(withId(R.id.button_playerMenu_myProfile)).perform(click());
+        Thread.sleep(1000);
+
+        // Press Generate QR Code button
+        Espresso.onView(withId(R.id.button_myProfile_generateQRCode)).perform(click());
+        Thread.sleep(1000);
+
+        // Check the QR Code
+        Espresso.onView(withId(R.id.MyProfileImageView)).check(matches(isDisplayed()));
+
+        scenario.close();
+    }
+
+    @Test
+    public void searchUserRCode() throws Exception {
+        // Launch MainActivity
+        ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
+
+        Espresso.onView(withId(R.id.sign_in_with_qrcode_button)).check(matches(isClickable()));
+        // Type "admin" to username editText
+        Espresso.onView(withId(R.id.signin_username_editText))
+                .perform(typeText("admin"));
+        // Type password
+        Espresso.onView(withId(R.id.signin_password_editText))
+                .perform(typeText("pwadmin"));
+
+        // Press button to sign in
+        Espresso.onView(withId(R.id.sign_in_button)).perform(click());
+        Thread.sleep(1000);
+
+        // Press search username button
+        Espresso.onView(withId(R.id.button_playerMenu_searchUsername)).perform(click());
+        Thread.sleep(1000);
+
+        // Check if QR code can be used to search username
+        Espresso.onView(withId(R.id.buttonQRCode_searchUsername)).check(matches(isClickable()));
+
+        scenario.close();
+    }
+
 }
